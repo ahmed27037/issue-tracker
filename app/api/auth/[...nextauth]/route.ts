e6337057ff907a98afa7f4db876d9a923ca8e8f2
ahmed27037 <ahmedamjedosman@gmail.com>
2023-12-11 14:37:07 +0400

    authoptions testing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 
-import { authOptions } from "../../authoptions/route";
+import { authOptions } from "../../authoptions/authoptions";
 import NextAuth from "next-auth";
 
 const handler =  NextAuth(authOptions);
