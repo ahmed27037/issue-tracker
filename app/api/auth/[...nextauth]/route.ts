6e68bbf3822d40dcdd2ea7d3dd2e6665e53175cf
ahmed27037 <ahmedamjedosman@gmail.com>
2023-12-11 14:31:37 +0400

    fix handler bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,7 @@
 
+import { authOptions } from "../../authoptions/route";
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-};
+const handler =  NextAuth(authOptions);
 
-const handler = NextAuth(authOptions);
-
-export default handler;
+export { handler as GET, handler as POST}
